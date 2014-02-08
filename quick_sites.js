23d325dfa5812df4dbae2b82f2d03b46bfafa535
Vedat Hallac <vedathallac@gmail.com>
2014-02-08 12:56:00 +0200

    Cleanup of sites that are no longer very common

--- quick_sites.js
+++ quick_sites.js
@@ -1,12 +1,10 @@
-interactive("open-gmail", "Go to gmail", "follow-new-buffer-background",
+interactive("open-gmail", "Go to gmail",
+            "follow-new-buffer-background",
             $browser_object = "http://gmail.com/");
 define_key(content_buffer_normal_keymap, "f1", "open-gmail");
 
-interactive("open-tda", "Go to gmail/TDA", "follow-new-buffer-background",
-            $browser_object = "http://mail.google.com/a/tda-tech.com/");
-define_key(content_buffer_normal_keymap, "f2", "open-tda");
-
-interactive("open-zebra", "Go to gmail/TDA", "follow-new-buffer-background",
-            $browser_object = "http://www.zebraguild.com/");
-define_key(content_buffer_normal_keymap, "f3", "open-zebra");
-
+interactive("open-outlook",
+            "Go to outlook",
+            "follow-new-buffer-background",
+            $browser_object = "http://outlook.com/");
+define_key(content_buffer_normal_keymap, "f2", "open-outlook");
