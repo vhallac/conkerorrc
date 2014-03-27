f76ff1b9992aa433e0194d1c3938bf13345c393f
Vedat Hallac <vedathallac@gmail.com>
2014-03-27 20:04:53 +0200

    Added missing semicolons

--- webjumps.js
+++ webjumps.js
@@ -24,20 +24,21 @@ define_webjump("imdb", "http://imdb.com/find?q=%s");
 define_webjump("rfc", "http://www.ietf.org/rfc/rfc%s.txt");
 define_webjump("github", "https://github.com/search?q=%s");
 /* StumbleUpon */
-define_webjump("stumble", "http://www.stumbleupon.com/to/stumble/go/")
+define_webjump("stumble", "http://www.stumbleupon.com/to/stumble/go/");
 define_webjump("stumble_like",
-               "javascript:document.location.href='http://www.stumbleupon.com/submit?url='+document.URL+'&title='+document.title.replace(/%20/g,'+');")
+               "javascript:document.location.href='http://www.stumbleupon.com/submit?url='+document.URL+'&title='+document.title.replace(/%20/g,'+');");
 define_webjump("stumble_reviews",
-               "javascript:(function(){url=document.location.href.split('://');document.location.href='http://www.stumbleupon.com/url/'+escape(url[1]);})();")
-define_webjump("deb-pkg", "http://packages.debian.org/search?keywords=%s&searchon=names&suite=stable&section=all")
-define_webjump("deb-pkg-all", "http://packages.debian.org/search?keywords=%s&searchon=names&suite=all&section=all")
-define_webjump("deb-pkg-backports", "http://packages.debian.org/search?keywords=%s&searchon=names&suite=squeeze-backports&section=all")
-define_webjump("deb-file", "http://packages.debian.org/search?searchon=contents&keywords=%s&mode=path&suite=stable&arch=any")
-define_webjump("deb-file-testing", "http://packages.debian.org/search?searchon=contents&keywords=%s&mode=path&suite=testing&arch=any")
-define_webjump("g", "https://www.google.com/search?q=%s")
-define_webjump("torrent", "http://kickass.to/usearch/%s")
-define_webjump("hepsi", "http://www.hepsiburada.com/liste/search.aspx?sText=%s")
-define_webjump("gitti", "http://www.gittigidiyor.com/arama/?k=%s&sra=hpd")
-define_webjump("sahibinden", "http://www.sahibinden.com/arama?query_text=%s")
+               "javascript:(function(){url=document.location.href.split('://');document.location.href='http://www.stumbleupon.com/url/'+escape(url[1]);})();");
+define_webjump("deb-pkg", "http://packages.debian.org/search?keywords=%s&searchon=names&suite=stable&section=all");
+define_webjump("deb-pkg-all", "http://packages.debian.org/search?keywords=%s&searchon=names&suite=all&section=all");
+define_webjump("deb-pkg-backports", "http://packages.debian.org/search?keywords=%s&searchon=names&suite=squeeze-backports&section=all");
+define_webjump("deb-file", "http://packages.debian.org/search?searchon=contents&keywords=%s&mode=path&suite=stable&arch=any");
+define_webjump("deb-file-testing", "http://packages.debian.org/search?searchon=contents&keywords=%s&mode=path&suite=testing&arch=any");
+define_webjump("g", "https://www.google.com/search?q=%s");
+define_webjump("torrent", "http://kickass.to/usearch/%s");
+define_webjump("hepsi", "http://www.hepsiburada.com/liste/search.aspx?sText=%s");
+define_webjump("gitti", "http://www.gittigidiyor.com/arama/?k=%s&sra=hpd");
+define_webjump("sahibinden", "http://www.sahibinden.com/arama?query_text=%s");
+define_webjump("minecraft", "http://minecraft.gamepedia.com/index.php?search=%s");
 /* default webjump */
 read_url_handler_list = [read_url_make_default_webjump_handler("google")];
